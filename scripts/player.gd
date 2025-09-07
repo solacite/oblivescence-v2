8e6a09fc0f7f268a55b19440e1b973a593232c52
solacite <julia.nguyen.do@gmail.com>
2025-09-06 23:02:11 -0400

    add player camera

--- scripts/player.gd
+++ scripts/player.gd
@@ -12,16 +12,38 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
+	# jump input
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# horizontal input
 	var direction := Input.get_axis("ui_left", "ui_right")
+	
+	# set horizontal velocity
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	# i like to move it move it
 	move_and_slide()
+	
+	# update character anim based on mvmt
+	update_animations()
+
+func update_animations():
+	var animated_sprite = $AnimatedSprite2D
+	
+	# jump/fall
+	if not is_on_floor():
+		if velocity.y < 0:
+			animated_sprite.play("jump")
+		else:
+			animated_sprite.play("fall")
+	
+	elif velocity.x != 0:
+		animated_sprite.play("run")
+		animated_sprite.flip_h = velocity.x < 0
+		
+	else:
+		animated_sprite.play("idle")
